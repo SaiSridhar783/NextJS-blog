2a350c220cba545583728257775ae991e2dce757
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-06-02 14:01:31 +0530

    Environent Variables added

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -24,10 +24,12 @@ async function handler(req, res) {
 
     let client;
 
+    const databaseUrl = `mongodb://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}-shard-00-00.dipfo.mongodb.net:27017,${process.env.mongodb_clustername}-shard-00-01.dipfo.mongodb.net:27017,${process.env.mongodb_clustername}-shard-00-02.dipfo.mongodb.net:27017/${process.env.mongodb_database}?ssl=true&replicaSet=atlas-jzsi0n-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
     try {
-      client = await MongoClient.connect(
-        "mongodb://sai:skgkmkg783@cluster0-shard-00-00.dipfo.mongodb.net:27017,cluster0-shard-00-01.dipfo.mongodb.net:27017,cluster0-shard-00-02.dipfo.mongodb.net:27017/my-site?ssl=true&replicaSet=atlas-jzsi0n-shard-0&authSource=admin&retryWrites=true&w=majority"
-      );
+      client = await MongoClient.connect(databaseUrl, {
+        useUnifiedTopology: true,
+      });
     } catch (error) {
       res.status(500).json({ message: "Could not connect to database." });
       return;
