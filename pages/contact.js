59f0022dbe7d7d1018c9c0b0082e6f94b9bd929d
SaiSridhar783 <sridhar.akula001@gmail.com>
2021-06-02 13:29:39 +0530

    Added Metadata

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,17 @@
 import ContactForm from "../components/contact/contact-form";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const Contact = () => {
-  return <ContactForm />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Contact Me</title>
+        <meta name="description" content="Contact me here" />
+      </Head>
+      <ContactForm />
+    </Fragment>
+  );
 };
 
 export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,18 @@ import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/Featured-Posts";
 import Hero from "../components/home-page/hero";
 import { getFeaturedPosts } from "../lib/posts-util";
+import Head from "next/head";
 
 const HomePage = ({ posts }) => {
   return (
     <Fragment>
+      <Head>
+        <title>Sai's Blog</title>
+        <meta
+          name="description"
+          content="I post about Web Developmenmt and Anime"
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={posts} />
     </Fragment>
